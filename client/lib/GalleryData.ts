794c6adf3a6d6ccaf3153496f8b50fb4215c89e5
TtheSebas <gabriel.meias@epn.edu.ec>
2026-09-06 17:23:31 -0500

    feat: add new images to process gallery

--- client/lib/GalleryData.ts
+++ client/lib/GalleryData.ts
@@ -299,6 +299,11 @@ export const GALLERIES: Record<string, { title: string; poster?: string; precioD
   proceso: {
     title: "Proceso de Fabricacion",
     images: [
+      { src: "img/proceso/proceso-nuevo-1.jpg", alt: "Fabricación de sillas en taller" },
+      { src: "img/proceso/proceso-nuevo-2.jpg", alt: "Base de mesa tallada" },
+      { src: "img/proceso/proceso-nuevo-3.jpg", alt: "Estructuras de sillas de madera" },
+      { src: "img/proceso/proceso-nuevo-4.jpg", alt: "Estructura de cama en el taller" },
+      { src: "img/proceso/proceso-nuevo-5.jpg", alt: "Armado de cama de madera" },
       { src: "img/proceso/157.webp", alt: "Proceso de fabricacion" },
       { src: "img/proceso/158.webp", alt: "Proceso de fabricacion" },
       { src: "img/proceso/223.webp", alt: "Proceso de fabricacion" },
